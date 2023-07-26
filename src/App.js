8acd96c2a93aee6180ea017a2548f277a7e538bc
goku-raj <anshuraj1479@gmail.com>
2023-07-26 14:24:52 +0530

    useState Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ num , setNum] = useState(0)
+  const increase =()=>{
+    return setNum( num +1);
+  };
+  const decrease =()=>{
+     return setNum( num -1);
+  };
+  const reset =()=>{
+    return setNum(0);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+      
+      <div>
+      {num}
+      </div>
     </div>
   );
 }
