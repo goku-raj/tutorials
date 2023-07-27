4a1b1929f56fb4a2d11a72bc83e1bec154af5a77
goku-raj <anshuraj1479@gmail.com>
2023-07-28 00:50:59 +0530

    useEffect

--- src/UseEffect.js
+++ src/UseEffect.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseEffect = () => {
+    const [clicked, setClicked] = useState();
+    const[count, setCount] = useState(0)
+
+    useEffect(()=>{
+        console.log('in use state')
+    },[ clicked,count ])
   return (
     <div>
-      
+      <button onClick={()=>setClicked('HELLO')}>HELLO</button>
+      <br/>
+      <hr/>
+      <button onClick={()=>setClicked('ME')}>ME</button>
+      <br/>
+      <hr/>
+      <button onClick={()=>setClicked('GOKU')}>GOKU</button>
+      <h1>{clicked}</h1>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <h2>{count}</h2>
     </div>
   )
 }
