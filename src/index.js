1aaf651c6f4d784add61071f92e47d98b5243759
goku-raj <anshuraj1479@gmail.com>
2023-08-02 23:15:43 +0530

    useState usecase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import Header from './Header';
 import Input from './component/Input';
+import MultipleReturnsFetchData from './component/UseState';
+import UserChallenge from './component/UserChallenge';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Header/>
-  <Input />
+
+  <UserChallenge />
+  
   </React.StrictMode>
 );
 
